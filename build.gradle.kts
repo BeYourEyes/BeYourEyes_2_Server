d305770405c1cb7e0aa03a242a9025c58e8618dc
Lee-SooJin <87466284+Soojin-Lee-01@users.noreply.github.com>
2025-03-05 12:16:49 +0900

    feat : firebase 연동 위한 config 파일 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,6 +46,8 @@ dependencies {
 	implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
 	implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
+	implementation("com.google.firebase:firebase-admin:9.2.0")
+
 
 }
 
---
+++ src/main/kotlin/com/beyoureyes/beyoureyes/config/FirebaseConfig.kt
@@ -0,0 +1,39 @@
+package com.beyoureyes.beyoureyes.config
+
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import com.google.firebase.cloud.StorageClient
+import com.google.cloud.storage.Bucket
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
+import java.io.IOException
+
+@Configuration
+class FirebaseConfig {
+
+    @Value("\${firebase.configuration-file}")
+    private lateinit var account: String
+
+    @Value("\${firebase.bucket}")
+    private lateinit var bucket: String
+
+    @Bean
+    @Throws(IOException::class)
+    fun firebaseApp(): FirebaseApp {
+        val options = FirebaseOptions.builder()
+            .setCredentials(GoogleCredentials.fromStream(ClassPathResource(account).inputStream))
+            .setStorageBucket(bucket)
+            .build()
+
+        return FirebaseApp.initializeApp(options)
+    }
+
+    @Bean
+    @Throws(IOException::class)
+    fun bucket(): Bucket {
+        return StorageClient.getInstance(firebaseApp()).bucket()
+    }
+}
