b38d778f1387c84f25157969a7882773739652b3
Lee-SooJin <87466284+Soojin-Lee-01@users.noreply.github.com>
2025-03-05 10:48:34 +0900

    feat : Swagger 추가 위한 dto 작성

--- src/main/kotlin/com/beyoureyes/beyoureyes/dto/VerifyRequestDto.kt
+++ src/main/kotlin/com/beyoureyes/beyoureyes/dto/VerifyRequestDto.kt
@@ -3,7 +3,7 @@ package com.beyoureyes.beyoureyes.dto
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotBlank
 
-data class  VerifyRequestDto(
+data class VerifyRequestDto(
     @Schema(description = "JWT 토큰", example = "your-jwt-token")
     @field:NotBlank(message = "jwt 토큰값은 필수 입력값입니다.") // 공백, 널 값 방지
     val token: String
