1029957204f96ff34ecb47e2846d32f09a976ebf
Soojin-Lee-01 <sojinlee1004@naver.com>
2025-03-22 15:47:57 +0900

    feat : 닉네임 중복 검사 API 응답 형식 변경

--- src/main/kotlin/com/beyoureyes/beyoureyes/controller/UserInfoController.kt
+++ src/main/kotlin/com/beyoureyes/beyoureyes/controller/UserInfoController.kt
@@ -131,7 +131,7 @@ class UserInfoController(
         return if (isAvailable) {
             ResponseEntity.ok(ResponseUtil.success("사용 가능한 닉네임입니다.", true))
         } else {
-            ResponseEntity.ok(ResponseUtil.error("이미 사용 중인 닉네임입니다.", false))
+            ResponseEntity.ok(ResponseUtil.success("이미 사용 중인 닉네임입니다.", false))
         }
     }
     @GetMapping("/info")
