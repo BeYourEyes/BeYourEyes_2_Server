0bbdf6c78f21f40809626c6251d5a70ffbfa6d29
Lee-SooJin <87466284+Soojin-Lee-01@users.noreply.github.com>
2025-03-25 11:16:33 +0900

    feat : 휴먼처리된 계정 처리

--- src/main/kotlin/com/beyoureyes/beyoureyes/mapper/UserMapper.kt
+++ src/main/kotlin/com/beyoureyes/beyoureyes/mapper/UserMapper.kt
@@ -20,8 +20,13 @@ interface UserMapper {
     @Select("SELECT user_id, device_id FROM \"user\"")
     fun findAll(): List<User>
 
-    @Select("SELECT user_id FROM \"user\" WHERE device_id = #{deviceId}")
+    @Select("""
+        SELECT user_id, device_id, last_login, deleted_at
+        FROM "user"
+        WHERE device_id = #{deviceId}
+    """)
     fun findByDeviceId(deviceId: String): User?
+
 
     @Insert("INSERT INTO \"user\" (device_id) VALUES (#{deviceId})")
     @Options(useGeneratedKeys = true, keyProperty = "userId")
--- src/main/kotlin/com/beyoureyes/beyoureyes/service/UserService.kt
+++ src/main/kotlin/com/beyoureyes/beyoureyes/service/UserService.kt
@@ -31,6 +31,17 @@ class UserService(private val userMapper: UserMapper, private val jwtUtil: JwtUt
                 .status(HttpStatus.NOT_FOUND)
                 .body(ResponseUtil.no_data("존재하지 않는 사용자입니다. 정보 저장이 필요합니다.", null))
         }
+        if (user.deletedAt != null) {
+            userMapper.reactivateUser(user.userId!!)
+
+            val accessToken = jwtUtil.generateAccessToken(user.userId)
+            val refreshToken = jwtUtil.generateRefreshToken(user.userId)
+            userMapper.updateRefreshToken(user.userId, refreshToken)
+
+            return ResponseEntity.ok(
+                ResponseUtil.active("휴먼처리된 계정에서 일반 계정으로 전환합니다.", accessToken)
+            )
+        }
 
         val accessToken = jwtUtil.generateAccessToken(user.userId!!)
         val refreshToken = jwtUtil.generateRefreshToken(user.userId)
